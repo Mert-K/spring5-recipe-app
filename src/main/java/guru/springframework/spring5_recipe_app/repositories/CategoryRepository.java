df0abb7f250ca75f8b4d7b6c582bb67573cb6c9b
Mert Kurt <tyrael91@hotmail.com>
2025-05-01 21:41:14 +0300

    Spring Data JPA Query Methods added to the CategoryRepository and UnitOfMeasureRepository.

--- src/main/java/guru/springframework/spring5_recipe_app/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/spring5_recipe_app/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package guru.springframework.spring5_recipe_app.repositories;
 import guru.springframework.spring5_recipe_app.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+    Optional<Category> findByDescription(String description);
 }
